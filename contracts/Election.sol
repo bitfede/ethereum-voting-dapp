b7e9e10691f9314325e738d44e42a15ddbe73d00
Fede Defaveri <f.g.defaveri@gmail.com>
2019-05-09 00:07:05 +0200

    Adds the contract file with a basic structure

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.2;
+
+contract Election {
+  //Read/Write candidate
+  string public candidate
+
+  //constructor
+  function Election() public {
+    candidate = "Candidate 1";
+  }
+}
