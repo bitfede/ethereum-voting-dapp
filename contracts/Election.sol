96319bca6cbcf2043ee7aa09058e92eb10510d6c
Fede Defaveri <f.g.defaveri@gmail.com>
2019-05-12 02:26:22 +0200

    Adds a struct to describe the candidates attributes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,15 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-  //Read/Write candidate
-  string public candidate;
 
-  //constructor
-  constructor() public {
-    candidate = "Candidate 1";
+  //model a candidate
+  struct Candidate {
+    uint sid;
+    string name;
+    uint voteCount;
   }
+
+
+
+
 }
